b9ddfe5f9f085075446f6ec92444bd4ac30c0038
David Kennedy <citizen@zenopolis.com>
2016-04-10 16:23:04 +0100

    Terminating: Better Shared Responsibility.

--- Shoebox/AppDelegate.swift
+++ Shoebox/AppDelegate.swift
@@ -28,7 +28,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func applicationShouldTerminate(sender: NSApplication) -> NSApplicationTerminateReply {
-        return dataController.applicationShouldTerminate(sender)
+        
+        do {
+            try dataController.terminate()
+        } catch {
+            let nserror = error as NSError
+            // Customize this code block to include application-specific recovery steps.
+            let result = sender.presentError(nserror)
+            if (result) {
+                return .TerminateCancel
+            }
+            
+            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
+            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
+            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
+            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
+            let alert = NSAlert()
+            alert.messageText = question
+            alert.informativeText = info
+            alert.addButtonWithTitle(quitButton)
+            alert.addButtonWithTitle(cancelButton)
+            
+            let answer = alert.runModal()
+            if answer == NSAlertFirstButtonReturn {
+                return .TerminateCancel
+            }
+        }
+        
+        // If we got here, it is time to quit.
+        return .TerminateNow
     }
 
     // MARK: - Data Saving and Undo support
--- Shoebox/DataController.swift
+++ Shoebox/DataController.swift
@@ -69,45 +69,27 @@ class DataController: NSObject {
         return managedObjectContext.undoManager
     }
     
-    func applicationShouldTerminate(sender: NSApplication) -> NSApplicationTerminateReply {
+    func terminate() throws {
         // Save changes in the application's managed object context before the application terminates.
         
         if !managedObjectContext.commitEditing() {
             NSLog("\(NSStringFromClass(self.dynamicType)) unable to commit editing to terminate")
-            return .TerminateCancel
+            let userInfo: [NSObject : AnyObject] = [NSLocalizedDescriptionKey : NSLocalizedString("DataController unable to commit editing for termination", comment: "Unable to to commit editing for termination informative error message")]
+            throw NSError(domain: "Shoebox", code: NSManagedObjectValidationError, userInfo: userInfo)
         }
         
         if !managedObjectContext.hasChanges {
-            return .TerminateNow
+            return
         }
         
         do {
             try managedObjectContext.save()
         } catch {
             let nserror = error as NSError
-            // Customize this code block to include application-specific recovery steps.
-            let result = sender.presentError(nserror)
-            if (result) {
-                return .TerminateCancel
-            }
-            
-            let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
-            let info = NSLocalizedString("Quitting now will lose any changes you have made since the last successful save", comment: "Quit without saves error question info");
-            let quitButton = NSLocalizedString("Quit anyway", comment: "Quit anyway button title")
-            let cancelButton = NSLocalizedString("Cancel", comment: "Cancel button title")
-            let alert = NSAlert()
-            alert.messageText = question
-            alert.informativeText = info
-            alert.addButtonWithTitle(quitButton)
-            alert.addButtonWithTitle(cancelButton)
-            
-            let answer = alert.runModal()
-            if answer == NSAlertFirstButtonReturn {
-                return .TerminateCancel
-            }
+            throw nserror
         }
         // If we got here, it is time to quit.
-        return .TerminateNow
+        return
     }
     
     // MARK: - Core Data stack
