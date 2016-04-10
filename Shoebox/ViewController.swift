aebab2d8e9dbc1369796f0039c6c975c829900b4
David Kennedy <citizen@zenopolis.com>
2016-04-10 14:52:02 +0100

    Initial Commit

---
+++ Shoebox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Shoebox
+//
+//  Created by David Kennedy on 10/04/2016.
+//  Copyright © 2016 David Kennedy of Zenopolis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
